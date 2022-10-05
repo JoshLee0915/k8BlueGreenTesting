6872d25f60acdb5bb799f86e793e1cea6b7705f3
JoshLee0915 <joshLee0915@gmail.com>
2022-10-05 15:51:37 -0600

    Added environment configuration to the api

--- test-api/src/app.controller.ts
+++ test-api/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, HttpException, HttpStatus} from '@nestjs/common';
 
 @Controller()
 export class AppController {
@@ -6,9 +6,16 @@ export class AppController {
 
   @Get('test')
   getTestRequest() {
+    const color = process.env.color ?? '#ffffff'
+    const version = process.env.version ?? '0.0'
+    const flaky = Number(process.env.flakiness) ?? 0.0
+
+    if(Math.random() < flaky)
+      throw new HttpException('OH NO I BECAME A TEAPOT!', HttpStatus.I_AM_A_TEAPOT)
+
     return {
-      color: '#0048ff',
-      version: '1.0'
+      color: color,
+      version: version
     };
   }
 }
